0819a07d2170802e1f617633660ff6405a2684c2
PotatoBob <league@WTS11.attlocal.net>
2016-03-27 19:00:17 -0700

    ArrayLists

---
+++ Level 3/src/Student.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Student {
+	private float intell;
+	String name;
+	Student(float intell, String name) {
+		this.intell = intell;
+		this.name = name;
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public float getIntell() {
+		return intell;
+	}
+
+}
